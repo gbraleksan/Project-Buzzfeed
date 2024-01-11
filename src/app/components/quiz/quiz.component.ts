81badf8924793b1395d5683b59ef1f3202f110e8
Aleksander <gbraleksan@gmail.com>
2024-01-11 19:52:12 -0300

    finalizando projeto

---
+++ src/app/components/quiz/quiz.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json"
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css']
+})
+export class QuizComponent implements OnInit {
+
+  title:string = "";
+  subtitle:string = "";
+  resultadoImagem: string = "";
+
+  questions:any;
+  questionSelected:any;
+
+  answers:string[] = [];
+  answerSelected:string ="";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false;
+      this.title = quiz_questions.title;
+      this.subtitle = quiz_questions.title; //ALTERAR ISSO AQUI PRA SUBTITLE
+      this.resultadoImagem = ""//quiz_questions.img;
+
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+        this.questionMaxIndex = this.questions.length;
+
+    }
+  }
+
+  selectedOption(value:string){
+    this.answers.push(value)
+    this.nextStep()
+  }
+
+  async nextStep(){
+    this.questionIndex++
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quiz_questions.results[
+        finalAnswer as keyof typeof quiz_questions.results ]
+    }
+  }
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr) =>{
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous;
+      }else{
+        return current;
+      }
+    })
+
+    return result;
+  }
+}
